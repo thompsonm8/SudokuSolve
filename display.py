a7f7ebb59ad3bda48f55e12b14bb2f346dd90ad8
thompsonm8 <thompsonm8@Linux.myguest.virtualbox.org>
2023-01-31 05:00:42 -0700

    Fixed a visual bug, added mouse functionality.

--- display.py
+++ display.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import sodokuSolve as s
+import sudokuSolve as s
 
 br = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
@@ -30,6 +30,12 @@ while not exit:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit = True
+
+        if event.type == pg.MOUSEBUTTONUP:
+            mousePOS = pg.mouse.get_pos()
+            x = mousePOS[0] // 100
+            y = mousePOS[1] // 100
+            print(f"X: {x}, Y: {y}, Value: {br[y][x]}")
     
     canvas.fill(WHITE)
     for x in range(9):
@@ -40,10 +46,10 @@ while not exit:
             pg.draw.line(canvas,BLACK, ((x*100),0),((x*100),900),3)
             pg.draw.line(canvas,BLACK, (0,(x*100)),(900,(x*100)),3)
     
-    for x in range(9):
-        for y in range(9):
-            if br[x][y] != 0:
-                num = font1.render(str(br[x][y]), True, BLACK)
+    for y in range(9):
+        for x in range(9):
+            if br[y][x] != 0:
+                num = font1.render(str(br[y][x]), True, BLACK)
                 canvas.blit(num,((x*100)+45,(y*100+45)))
 
     #br = s.solve(br)
